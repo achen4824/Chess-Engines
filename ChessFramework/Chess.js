3117e5d196f533cd62a02350b420539a1db46ee8
Andrew Chen <achen4824@gmail.com>
2019-11-25 14:15:36 +1030

    Enabled Drag and selection capability for movement

--- ChessFramework/Chess.js
+++ ChessFramework/Chess.js
@@ -1,12 +1,34 @@
 
-$('.gameCell').click(function(){
-    var posX = 0, posY=0, posTempX=0, posTempY=0;
+
+$('.gameCell').mousedown(function(event){
+     //get mouse position
+    var currentMousePos = { x: -1, y: -1 };
+    currentMousePos.x = event.pageX;
+    currentMousePos.y = event.pageY;
+
+    var offsetX=0, offsetY=0;
     $('#selector').css("display","block");
     var rect = this.getBoundingClientRect();
     $('#selector').css("top",(rect.top-8)+"px");
     $('#selector').css("left",(rect.left-8)+"px");
-    console.log(rect.top-8,rect.left-8);
+
+    offsetY  = (rect.bottom-rect.top)/2+8;
+    offsetX  = (rect.right-rect.left)/2+8;
+
+    $('#selector').mousemove(function(currEvent){
+        $('#selector').css("top",(currEvent.pageY - offsetY)+"px");
+        $('#selector').css("left",(currEvent.pageX - offsetX)+"px");
+    }).mouseup(function(currEvent){
+        $('#selector').off("mousemove");
+        $(document).off("mousemove");
+
+        //snap to box
+        $('#selector').css("top",(currEvent.pageY-(currEvent.pageY%48))+"px");
+        $('#selector').css("left",(currEvent.pageX-(currEvent.pageX%48))+"px");
+    });
 });
+
+
 
 $(document).ready(function() {
 
